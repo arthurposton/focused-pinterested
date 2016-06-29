532f9c85df779b21a23ac7bea01030341f6d8c0a
Arthur G. Poston Jr <arthur.poston@focuseddevelopment.net>
2016-06-29 16:55:24 +0000

    Pinterested Second Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'home#index'
+  get 'home/about'
+  get 'users/sign_up'
+  get 'users/sign_in'
+  get 'users/edit'
+  get 'users/password/new'
+  get 'users/password/edit'
+  get 'users/cancel'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
